340d1c3b982f4e6a8ebfcad1a7a33ccde0297015
applejuice <hkaraoglu@mehmetakif.edu.tr>
2023-03-29 22:02:48 +0300

    Initial commit

---
+++ frontend/src/components/GuestLayout.jsx
@@ -0,0 +1,12 @@
+import {Outlet} from "react-router-dom";
+
+const GuestLayout = () => {
+  return (
+    <div>
+      Guest Layout
+      <Outlet />
+    </div>
+  )
+}
+
+export default GuestLayout
